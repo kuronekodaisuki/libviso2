1aa83ea3325013b61a94c438843a5e17f5793ce4
Hiroshi Matsuoka <pipomaomy2005@gmail.com>
2017-06-18 12:59:40 +0900

    modify demo_with_OvrvisionPro

--- build/demo_with_OvrvisionPro/demo_with_OvrvisionPro.cpp
+++ build/demo_with_OvrvisionPro/demo_with_OvrvisionPro.cpp
@@ -45,43 +45,58 @@ Usage: ./viso2 path/to/sequence/2010_03_09_drive_0019
 #include <opencv2\core.hpp>
 #include <opencv2\imgcodecs.hpp>
 #include <opencv2\imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #pragma warning(pop)
 
 #include <ovrvision_pro.h>
 
 using namespace std;
 
-
 int main(int argc, char** argv)
 {
-	// we need the path name to 2010_03_09_drive_0019 as input argument
-	if (argc<2) {
-		cerr << "Usage: ./viso2 path/to/sequence/2010_03_09_drive_0019" << endl;
-		return 1;
-	}
+	OVR::OvrvisionPro ovrvision;	// OvrvisionPro camera
+	if (ovrvision.CheckGPU())
+	{
+		// Open with OpenGL sharing mode
+		if (ovrvision.Open(0, OVR::OV_CAMHD_FULL, 0) == 0)
+			puts("Can't open OvrvisionPro");
+		printf_s("Focal point: %f\n", ovrvision.GetCamFocalPoint());
+		int height = ovrvision.GetCamHeight();
+		int width = ovrvision.GetCamWidth();
+		OVR::ROI roi = { 0, 0, width, height };
+		cv::Mat left, right;
+		left.create(height, width, CV_8UC4);
+		right.create(height, width, CV_8UC4);
 
-	// sequence directory
-	string dir = argv[1];
+		// set most important visual odometry parameters
+		// for a full parameter list, look at: viso_stereo.h
+		VisualOdometryStereo::parameters param;
 
-	// set most important visual odometry parameters
-	// for a full parameter list, look at: viso_stereo.h
-	VisualOdometryStereo::parameters param;
+		// calibration parameters for sequence 2010_03_09_drive_0019 
+		param.calib.f = ovrvision.GetCamFocalPoint();	// focal length in pixels
+		param.calib.cu = width / 2;						// principal point (u-coordinate) in pixels
+		param.calib.cv = height / 2;					// principal point (v-coordinate) in pixels
+		param.base = 0.06;								// baseline in meters
 
-	// calibration parameters for sequence 2010_03_09_drive_0019 
-	param.calib.f = 645.24; // focal length in pixels
-	param.calib.cu = 635.96; // principal point (u-coordinate) in pixels
-	param.calib.cv = 194.13; // principal point (v-coordinate) in pixels
-	param.base = 0.5707; // baseline in meters
+		VisualOdometryStereo viso(param);				// init visual odometry
 
-						 // init visual odometry
-	VisualOdometryStereo viso(param);
+		// current pose (this matrix transforms a point from the current
+		// frame's camera coordinates to the first frame's camera coordinates)
+		Matrix pose = Matrix::eye(4);
+		for (bool loop = true; loop; )
+		{
+			ovrvision.Capture(OVR::Camqt::OV_CAMQT_DMS);
+			ovrvision.GetStereoImageBGRA(left.data, right.data, roi);
+			cv::imshow("Left", left);
+			cv::imshow("Right", right);
 
-	// current pose (this matrix transforms a point from the current
-	// frame's camera coordinates to the first frame's camera coordinates)
-	Matrix pose = Matrix::eye(4);
-
-	// loop through all frames i=0:372
-	for (int32_t i = 0; i < 1400; i++) {
+			switch (cv::waitKey(10))
+			{
+			case 'q':
+				loop = false;
+				break;
+			}
+		}
 	}
     return 0;
 }
